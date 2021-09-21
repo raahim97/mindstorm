9255147a872aba71bef3d5740a50291c4aa5645c
Raahim Naeem <raahim.naeem@yahoo.com>
2021-09-21 18:18:48 +0500

    Initial commit

---
+++ quiz/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from quiz.models import Course, Question
+# Register your models here.
+admin.site.register(Question)
+admin.site.register(Course)
---
+++ student/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from student import views
+from django.contrib.auth.views import LoginView
+from student.urls import *
+from student.views import *
+
+# app_name = 'student'
+
+urlpatterns = [
+path('studentclick', views.studentclick_view),
+path('studentlogin', views.login_attempt,name='studentlogin'),
+path('studentsignup', views.student_signup_view,name='studentsignup'),
+path('student-dashboard', views.student_dashboard_view,name='student-dashboard'),
+path('student-exam', views.student_exam_view,name='student-exam'),
+path('take-exam/<int:pk>', views.take_exam_view,name='take-exam'),
+path('start-exam/<int:pk>', views.start_exam_view,name='start-exam'),
+
+path('calculate-marks', views.calculate_marks_view,name='calculate-marks'),
+path('view-result', views.view_result_view,name='view-result'),
+path('check-marks/<int:pk>', views.check_marks_view,name='check-marks'),
+path('student-marks', views.student_marks_view,name='student-marks'),
+]
